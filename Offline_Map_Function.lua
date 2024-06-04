e366d6a850f508c52c4f7d587d0a616ac6b0ddb2
doubleruri <doubleruri@gmail.com>
2024-06-05 00:18:36 +0800

    Update Offline_Map_Function.lua

--- Offline_Map_Function.lua
+++ Offline_Map_Function.lua
@@ -11,8 +11,11 @@
 --房間移動方式的function(也許用個參數好切換)
 --回到現實世界地圖的function
 Offline_MAP = {}
-Offline_MAP.setting = ""
+Offline_MAP.setting = {}
+Offline_MAP.setting.pattern = 2
+Offline_MAP.setting.movemethod = "1"
 Offline_MAP.currentRoom = ""
+Offline_MAP.WhatInRoom = {"description","NPC","OBJ","Notes"}
 
 --播報員的function
 local MapperVoice = "<112,229,0>[Offline_MAP]: <255,255,255>"
@@ -35,6 +38,48 @@ function LoginOffLine(RoomID)
     Offline_MAP.currentRoom = RoomID
   end
     Offline_MAP.echo("Default Room is Here you are : "..Offline_MAP.currentRoom)
+	decho(MapperVoice)
+	echo("要從這裡開始嗎?  ")
+	dechoLink("<34,139,34>[<u> YES </u>]", [[display("YES")]],"", true)
+	echo("  ")
+  dechoLink("<255,0,255>[<u> NO </u>]", [[display("No")]],"", true)
     centerview(Offline_MAP.currentRoom)
 end
 ----------
+function DataLoad(RoomID)
+decho("<255,255,0>"..getRoomName(RoomID).."<255,255,255>\n")
+--顯示RoomUserData,依照Offline_MAP.WhatInRoom裡的順序
+for k,v in pairs (Offline_MAP.WhatInRoom) do
+  local results = getRoomUserData(RoomID,v,true)
+  if  results then echo(results.."\n\n")   end
+end
+--顯示出口
+local exits, special_exits = getRoomExits(RoomID),getSpecialExits(RoomID)
+decho("<255,255,0>出口：\n")
+if Offline_MAP.setting.pattern == 2 then
+  for dir,id in pairs (exits) do
+    	dechoLink("<34,139,34>"..dir.."<192,192,192>－"..getRoomName(id).."\n", function() centerview(id) DataLoad(id) end,"", true)
+  end
+  for goRoom,v in pairs (special_exits) do
+    for command,order in pairs(v) do
+      dechoLink("<34,139,34>"..command.."<192,192,192>－"..getRoomName(goRoom).."\n", function() centerview(goRoom) DataLoad(goRoom) end,"", true)
+    end
+  end
+else
+  local NumberOfExits = 0
+    --一般出口
+  for dir,id in pairs (exits) do
+    if NumberOfExits >0 then echo(" , ") end
+    dechoLink("<34,139,34>"..dir.."<192,192,192>", function() centerview(id) DataLoad(id) end,getRoomName(id), true)
+    NumberOfExits = NumberOfExits + 1
+  end
+  --抓取特殊出口--
+  for goRoom,v in pairs (special_exits) do
+    for command,order in pairs(v) do
+      echo(" , ")
+      dechoLink("<34,139,34>"..command.."<192,192,192>", function() centerview(goRoom) DataLoad(goRoom) end,getRoomName(goRoom), true)
+    end
+  end
+end
+echo("\n\n")
+end
